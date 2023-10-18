b65000da05bc683004637d5267079c88396feb5b
Fani Tarantsopoulou <fanitarants@gmail.com>
2023-10-18 17:00:29 +0300

    Update CPU.java

--- src/main/java/CPU.java
+++ src/main/java/CPU.java
@@ -1,19 +1,16 @@
 package main.java;
 
 public class CPU {
-    float speed;
-    String type;
+    private final double speed;
+    private final String type;
 
+    public CPU(double speed, String type) {
+        this.speed = speed;
+        this.type = type;
+    }
 
     @Override
     public String toString () {
-        return "CPU{" +
-                "speed=" + speed +
-                "type='" + type + '\'' +
-                '}';
-    }
-    public CPU(float speed, String type) {
-        this.speed = speed;
-        this.type = type;
+        return "CPU{" + "speed=" + speed + "type='" + type + '\'' + '}';
     }
 }
